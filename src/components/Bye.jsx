f0e06b90193a370cbb5a0256e7c57eccd0a301b4
mabbakk <siyun145928@gmail.com>
2025-02-03 11:37:25 +0900

    #1-4. 지출 항목 컴포넌트 생성

---
+++ src/components/Bye.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+import Greet from "./Greet.jsx";
+
+// 함수형 컴포넌트 : 최근 리액트 선호 방식
+function Bye() {
+
+    return (
+        <div>
+            <a href="https://www.naver.com">네이버로 이동!</a>
+            <Greet />
+        </div>
+    )
+}
+
+export default Bye;
---
+++ src/components/Greet.jsx
@@ -0,0 +1,18 @@
+// 인텔리제이 얼티밋에서는 rsc 를 입력하면 화살표 함수를 자동으로 만들어줌!
+// rsc
+
+import React from 'react';
+
+const Greet = () => {
+    return (
+        <div>
+            <ul>
+                <li>사과</li>
+                <li>오렌지</li>
+                <li>딸기</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Greet;
